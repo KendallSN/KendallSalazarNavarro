66ddef134216bface6a96235575ee179831d305b
Kendall Salazar Navarro <162237900+KendallSN@users.noreply.github.com>
2025-06-29 12:11:28 -0600

    Revert "Update astro.config.mjs"
    This reverts commit 88d444e18245e3647ced99da3ac93b15a7aa79c5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,11 +9,11 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src'),
-        '@/components': path.resolve(__dirname, 'src/components'),
-        '@/layouts': path.resolve(__dirname, 'src/layouts'),
-        '@/pages': path.resolve(__dirname, 'src/pages'),
-        '@/assets': path.resolve(__dirname, 'src/assets')
+        '@': '/src',
+        '@/components': '/src/components',
+        '@/layouts': '/src/layouts',
+        '@/pages': '/src/pages',
+        '@/assets': '/src/assets'
       }
     }
   }
